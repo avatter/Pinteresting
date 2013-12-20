d448daa483453a1f2339deae4e596b18d47b04d7
Andre Vatter <mail@avatter.de>
2013-12-20 22:46:53 +0100

    Erste Anpassungen

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get git" do
+    get :git
+    assert_response :success
+  end
+
+  test "should get add" do
+    get :add
+    assert_response :success
+  end
+
+end
