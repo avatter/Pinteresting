c421c210db9a46e0145b563dcba664a25f6b715e
Andre Vatter <mail@avatter.de>
2014-01-05 16:10:24 +0100

    vor den messages

--- app/models/adresse.rb
+++ app/models/adresse.rb
@@ -1,12 +1,10 @@
 class Adresse < ActiveRecord::Base
 	geocoded_by :adresse
-  validates_presence_of :strasse, :hausnummer, :plz, :stadt
+  validates_presence_of :strasse, :hausnummer, :stadt
 
 validates_numericality_of :plz, in: 1001..9999
-validates_numericality_of :plz, greater_than_or_equal_to: 1001, less_than_or_equal_to: 9999
-#validates_numericality_of :plz, :only_integer
-#validates_numericality_of :plz, :greater_than => 1001
-  #validates_numericality_of :plz :less_than_or_equal_to => 9999
+#validates_numericality_of :plz, greater_than_or_equal_to: 1001, less_than_or_equal_to: 9999
+
 
 
 
@@ -14,7 +12,7 @@ validates_numericality_of :plz, greater_than_or_equal_to: 1001, less_than_or_equ
 def adresse
  [strasse, hausnummer, plz, stadt].compact.join(', ')
 end
-after_validation :geocode, :if => :strasse_changed?
+after_validation :geocode #, :if => :strasse_changed?
 
 
 
